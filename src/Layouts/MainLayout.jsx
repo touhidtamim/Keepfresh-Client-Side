817a915889bed31bc28410ff0bfcd729bebd43a8
Touhid Tamim <touhidtamim1@gmail.com>
2025-06-13 15:24:42 +0600

    Complete main layout with header, footer, and outlet setup

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,24 @@
-import React from "react";
 import Spinner from "../Components/Spinner";
+import Navbar from "../Components/Navbar";
+import ScrollToTop from "./../Components/ScrollToTop";
+import Footer from "../Components/Footer";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div>
-      Main Latyout
-      <Spinner></Spinner>
-    </div>
+    <>
+      <Navbar></Navbar>
+
+      <ScrollToTop></ScrollToTop>
+
+      {navigation.state === "loading" && <Spinner />}
+
+      <div className="md:w-11/12 mx-auto min-h-[90vh]">
+        <Outlet />
+      </div>
+
+      <Footer></Footer>
+    </>
   );
 };
 
