43f7517461dfcaed9b7afd589060bafd704d4e7f
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-12 23:58:53 +0600

    fix bugs

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
