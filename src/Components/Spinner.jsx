20af8299a287159a27aefdd2c8d2c868a80ed185
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-13 23:28:22 +0600

    spinner updated

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -4,7 +4,7 @@ import { HashLoader } from "react-spinners";
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center h-screen w-full">
-      <HashLoader color="#2f8db5" size={70} />
+      <HashLoader color="#2f8db5" size={80} />
     </div>
   );
 };
